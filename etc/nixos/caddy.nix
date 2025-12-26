110958baef9311a523250b050a48f837ff98bb5d
Chris Mann <chris@lesgtrandsvoisins.com>
2025-12-26 12:08:44 +0100

    max.gdvoisins.com

--- etc/nixos/caddy.nix
+++ etc/nixos/caddy.nix
@@ -93,13 +93,14 @@ in
       "max.gdvoisins.com" = {
         extraConfig = ''
           authorize with identifiedpolicy
-          reverse_proxy https://max.local:8443 {
-            transport http {
-              tls_server_name max.local
-              tls_insecure_skip_verify
-              tls_client_auth /var/run/dashy/ssl/cert.pem /var/run/dashy/ssl/key.pem
-            }
-          }
+          reverse_proxy http://127.0.0.1:8080
+          # reverse_proxy https://max.local:8443 {
+          #   transport http {
+          #     tls_server_name max.local
+          #     tls_insecure_skip_verify
+          #     tls_client_auth /var/run/dashy/ssl/cert.pem /var/run/dashy/ssl/key.pem
+          #   }
+          # }
         '';
       };
     };
